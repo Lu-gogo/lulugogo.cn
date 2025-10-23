12d4dc775e3f5ab687efc7fe43628f785d8f9b9b
Lu-gogo <XI-lank@outlook.com>
2025-10-23 22:20:02 +0800

    修改vite配置2.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
@@ -12,6 +11,7 @@ export default defineConfig({
     },
   },
   build: {
+    minify: 'esbuild', // 使用 ESBuild 的默认压缩设置
     assetsInlineLimit: 4096,
     rollupOptions: {
       output: {
@@ -34,9 +34,6 @@ export default defineConfig({
         }
       },
     },
-    // 简化压缩配置 - 让 Vite 使用默认的 terser 配置
-    minify: 'terser',
-    // 移除有问题的 terserOptions，使用默认配置
   },
   optimizeDeps: {
     include: ['react', 'react-dom'],
